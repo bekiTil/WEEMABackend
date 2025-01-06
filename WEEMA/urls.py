fec4f9e5578656333e39804a51639f2b7f043bb8
Beimnet <beimnetbekele123@gmail.com>
2025-01-06 11:21:06 +0300

    fixed import issue

--- WEEMA/urls.py
+++ WEEMA/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 )
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
