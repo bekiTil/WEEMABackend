ad8ce8363bd3fabe0f9ca5a1b2ff2a00f25c38d5
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2025-03-09 10:48:15 +0300

    Update urls.py

--- WEEMA/urls.py
+++ WEEMA/urls.py
@@ -30,7 +30,8 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,)
+    permission_classes=(permissions.AllowAny,),
+    url='https://community.weema.org/api/v1/user/'
 )
 
 from django.contrib import admin
