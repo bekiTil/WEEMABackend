e7c99e02839c6181c6c24855e23bd8286280f8fc
Beimnet <beimnetbekele123@gmail.com>
2025-05-01 22:16:58 +0300

    feat: paginated memeber list and updated dump all data feature

--- cluster_management/urls.py
+++ cluster_management/urls.py
@@ -1,12 +1,13 @@
 # urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ClusterViewSet, SelfHelpGroupViewSet, MemberViewSet, TransferGroupsAPIView, GetListOfLocaton
+from .views import ClusterViewSet, SelfHelpGroupViewSet, MemberViewSet, TransferGroupsAPIView, GetListOfLocaton,PaginatedMemberViewSet
 
 router = DefaultRouter()
 router.register(r'clusters', ClusterViewSet)
 router.register(r'self-help-groups', SelfHelpGroupViewSet)
 router.register(r'members', MemberViewSet)
+router.register(r'paginated-members', PaginatedMemberViewSet, basename='paginated-member')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
